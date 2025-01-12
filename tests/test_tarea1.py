450e4132fb3a106a584de4539aafe2b7f5d67438
Hector Tenezaca <h.tenezaca@uniandes.edu.co>
2025-01-12 16:34:54 -0500

    fix: test

--- tests/test_tarea1.py
+++ tests/test_tarea1.py
@@ -2,8 +2,9 @@ import os
 from solutions.tarea1_solucion import detect_edges
 
 def test_detect_edges():
-    input_image = "assignments/task1/input_data/image1.jpg"
-    output_image = "assignments/task1/output_data/image1.jpg"
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    input_image = os.path.join(base_path, "../tasks/tarea1/input_data/lena.png")
+    output_image = os.path.join(base_path, "./tasks/tarea1/output_data/lena_output.png")
     detect_edges(input_image, output_image)
 
     assert os.path.exists(output_image), "La imagen de salida no se generó."
